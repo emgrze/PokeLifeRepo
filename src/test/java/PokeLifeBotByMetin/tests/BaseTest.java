9fb6b7c62e7873c2d034a919c8f80bdef63fca84
Metin <littlesweetream@interia.eu>
2023-06-05 23:23:37 +0200

    login test v0.1 added

--- src/test/java/PokeLifeBotByMetin/tests/BaseTest.java
+++ src/test/java/PokeLifeBotByMetin/tests/BaseTest.java
@@ -1,22 +1,29 @@
 package PokeLifeBotByMetin.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    protected WebDriver driver;
 
+    WebDriver driver = new EdgeDriver();
     @BeforeMethod
     public void setup() {
-        WebDriverManager.edgedriver().setup();
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.get("https://pokelife.pl/");
         driver.manage().window().maximize();
     }
+
+
+
 
     @AfterMethod
     public void tearDown() {
---
+++ src/test/java/PokeLifeBotByMetin/tests/LogInTest.java
@@ -0,0 +1,26 @@
+package PokeLifeBotByMetin.tests;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class LogInTest extends BaseTest {
+
+    @FindBy(name = "login")
+    private WebElement loginInput;
+
+    @FindBy(name = "haslo")
+    private WebElement passInput;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    private WebElement logInButton;
+
+    @Test
+    public void logInTest()  {
+        PageFactory.initElements(driver,this);
+        loginInput.sendKeys("lol");
+        passInput.sendKeys("lol123");
+        logInButton.click();
+    }
+}
